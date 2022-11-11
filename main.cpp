868973963e72dbba1c599229e3224dc253f03572
ilyassx1234 <117658214+ilyassx1234@users.noreply.github.com>
2022-11-11 16:12:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
+ #include <iostream>
+ using namespace std;
 
-int main() 
-   cout << "Hello, World!"<<end;
-
-}
+ int main() {
+   cout <<  "Hello, World!"<<end1;
+   return 0;
+ }
